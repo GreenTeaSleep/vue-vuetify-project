aa14e27bd28e2693aaeabd6e0eda0afc1aebeb77
Jaruwat Thongchana <jaruwat.works@gmail.com>
2023-01-13 06:22:47 +0700

    refactor: use webfontloader instead

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,4 @@
 </template>
 
 <script lang="ts" setup>
-//
 </script>
-
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap");
-* {
-  font-family: "Noto Sans Thai", sans-serif;
-}
-</style>
