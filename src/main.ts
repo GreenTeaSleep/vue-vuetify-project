121b8651178562c854f8cfb864a6e46f20737645
Radis Lamsuneekan <radisworldclassic.gm@gmail.com>
2023-01-25 12:00:40 +0700

    forget: use style

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { registerPlugins } from "@/plugins";
 
 import App from "./App.vue";
+import "./styles/main.css";
 
 const app = createApp(App);
 registerPlugins(app);
