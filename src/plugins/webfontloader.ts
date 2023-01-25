749169b1d798014066eb537187b3582d0c11c00d
Radis Lamsuneekan <radisworldclassic.gm@gmail.com>
2023-01-25 11:58:07 +0700

    backward: style

--- src/plugins/webfontloader.ts
+++ src/plugins/webfontloader.ts
@@ -1,21 +1,9 @@
-/**
- * plugins/webfontloader.ts
- *
- * webfontloader documentation: https://github.com/typekit/webfontloader
- */
-
 export async function loadFonts() {
-  const webFontLoader = await import(
-    /* webpackChunkName: "webfontloader" */ "webfontloader"
-  );
+  const webFontLoader = await import("webfontloader");
 
   webFontLoader.load({
     google: {
-      families: ["Noto Sans Thai:400", "sans-serif"],
-    },
-    custom: {
-      families: ["Noto Sans Thai", "sans-serif"],
-      urls: ["/src/style/main.css"],
+      families: ["Noto Sans Thai:100,300,400,500,700,900&display=swap"],
     },
   });
 }
